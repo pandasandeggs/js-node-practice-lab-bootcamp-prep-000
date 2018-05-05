4712454aedad8e07f65f9a396679924ccf740280
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-05 04:11:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Chantal"
-var height = "66"
+var height = 66
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 
+console.log(parseInt(height) + 1) 
 
